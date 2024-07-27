ffc249423d659840e09690d7c0d8dd19a9de24b2
Bhavesh Choudhary <bhaveshchoudhary0909@gmail.com>
2024-07-27 18:21:34 +0530

    all erros removed

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,8 +1,8 @@
 import { PrismaClient } from '@prisma/client';
 
-const globalForPrisma = global;
+const globalForPrisma = global as typeof globalThis & { prisma?: PrismaClient };
 
-const prisma = new PrismaClient();
+const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
