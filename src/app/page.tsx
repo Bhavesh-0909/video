1c444c2fbc6c7164a328a90a8a1a300f3371ae1e
Bhavesh Choudhary <bhaveshchoudhary0909@gmail.com>
2024-07-30 17:24:09 +0530

    localhost is up and working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 'use client';
 import { useSession } from "next-auth/react";
-import Image from "next/image";
 
 export default function Home() {
-  // const { data } = useSession();
-  // console.log(data);
+  const data = useSession();
+  console.log(data);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       home
